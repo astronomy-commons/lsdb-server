a52a7f18bf8b23fea7fa7caf7915fd9b0af7631c
schwarzam <gustavo.b.schwarz@gmail.com>
2024-04-23 16:43:49 +0000

    testing

--- tests/parquet.rs
+++ tests/parquet.rs
@@ -8,23 +8,20 @@ mod parser {
 
     use super::*;
 
-    #[test]
-    fn test_read_file() {
-        
+    #[tokio::test]
+    async fn test_read_file() {
         let file_path = paths::get_testdata_path("Npix=754.parquet");
         let mut params = HashMap::new();
 
         params.insert("cols".to_string(), "RA,DEC".to_string());
         params.insert("query".to_string(), "RA>=30.1241,DEC<=-30.3".to_string());
 
-
-
-        parquet::process_and_return_parquet_file_lazy(
+        let result = parquet::process_and_return_parquet_file_lazy(
             file_path.to_str().unwrap(), 
             &params
-        );
+        ).await;
 
-
+        // Add assertions here to verify the result
     }
     
 
