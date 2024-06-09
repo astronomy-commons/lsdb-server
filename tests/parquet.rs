44b7a56e5dcdfac3ab67af1eb77a8cc846334746
schwarzam <gustavo.b.schwarz@gmail.com>
2024-06-09 02:08:15 -0300

    fixed error on parse filter regex

--- tests/parquet.rs
+++ tests/parquet.rs
@@ -13,7 +13,7 @@ mod parser {
         let mut params = HashMap::new();
 
         params.insert("columns".to_string(), "RA,DEC".to_string());
-        params.insert("filters".to_string(), "RA>=30.1241,DEC<=-30.3,RA>30,DEC<-30,RA==1,RA=1,RA!=0".to_string());
+        params.insert("filters".to_string(), "RA>=30.1241,DEC<=-30.3,RA>30,DEC<=30,RA==1,RA=1,RA!=0".to_string());
 
         let result = parquet::parquet::process_and_return_parquet_file(
             file_path.to_str().unwrap(), 
