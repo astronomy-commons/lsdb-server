53f11d88b475f6be274d6d55ec36c2fe5214f30f
schwarzam <gustavo.b.schwarz@gmail.com>
2024-06-09 02:01:12 -0300

    updated tests

--- tests/parquet.rs
+++ tests/parquet.rs
@@ -12,8 +12,8 @@ mod parser {
         let file_path = paths::get_testdata_path("Npix=754.parquet");
         let mut params = HashMap::new();
 
-        params.insert("cols".to_string(), "RA,DEC".to_string());
-        params.insert("query".to_string(), "RA>=30.1241,DEC<=-30.3".to_string());
+        params.insert("columns".to_string(), "RA,DEC".to_string());
+        params.insert("filters".to_string(), "RA>=30.1241,DEC<=-30.3,RA>30,DEC<-30,RA==1,RA=1,RA!=0".to_string());
 
         let result = parquet::parquet::process_and_return_parquet_file(
             file_path.to_str().unwrap(), 
